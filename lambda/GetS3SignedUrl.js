986373dafb970996d6639bba4bfb63c6e49336f9
Sam Martin <samjackmartin@gmail.com>
2015-08-29 23:57:48 +0100

    Added some validation

--- lambda/GetS3SignedUrl.js
+++ lambda/GetS3SignedUrl.js
@@ -1,21 +1,29 @@
 var aws = require('aws-sdk');
 var s3 = new aws.S3({apiVersion: '2006-03-01'});
+var urlPrefix = 'https://s3-eu-west-1.amazonaws.com/image-upload-smartin/'
+var key = (new Date()).toISOString().substr(0,10) +'/' + String( Date.now())
 console.log('Loading event');
             
 exports.handler = function(event, context) {
     
-    keyname = Date.now()
+    if(!event.hasOwnProperty("contentType")){
+        context.fail("no content type specified")
+    }else if(!event.contentType.match(/(\.|\/)(gif|jpe?g|png)$/i)){
+        context.fail("invalid content type, gif, jpg, and png supported");
+    }
+    
+    console.log("Event Type: "+event.contentType)
     var params = {
-    	Bucket: 'image-upload-smartin', 
-    	Key: keyname,
-    	ACL:'public-read',
-    	Body: '',
+        Bucket: 'image-upload-smartin', 
+        Key: key,
+        ACL:'public-read',
+        Body: '',
         ContentType: event.contentType,
-    	Expires: 60
+        Expires: 60
     };
     
     s3.getSignedUrl('putObject', params, function (err, url) {
-    	console.log('The URL is', url);
-    	context.done(null, {"url":url}); // SUCCESS with message
+        console.log('The URL is', url);
+          context.done(null, {"oneTimeUploadUrl":url, "resultUrl":urlPrefix+key}); // SUCCESS with message
     })
 }
